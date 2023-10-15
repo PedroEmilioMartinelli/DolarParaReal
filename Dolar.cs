2fe9394765b6f131ad93ec73cfcdf26c362a97dd
Pedro Emilio Martinelli <pedroemiliomartinelli@protonmail.com>
2023-10-15 16:59:09 -0300

    first commit

---
+++ Dolar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cotaç_aoDolar
+{
+    internal class Dolar
+    {
+        public static double Iof = 5.00;
+
+        public static double DolarParaReal(double quantia, double cotaco)
+        {
+            double total = quantia * cotaco;
+            return total + total * Iof / 100.00;
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cotaç_aoDolar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual a cotação do dolar: ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dolares voce quer comprar? ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double result = Dolar.DolarParaReal(quantia, cotacao);
+            Console.WriteLine("Valor a ser pago em reais = " + result.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
